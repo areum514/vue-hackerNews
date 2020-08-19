3760aafa0ad137ed697c6c95f71b2400d333b522
areum514 <21500514@handong.edu>
2020-08-19 19:53:46 +0900

    first commit

---
+++ vue-news/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+
+    <router-view></router-view>
+  </div>
+  <!-- test -->
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style>
+</style>
---
+++ vue-news/src/views/NewsView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    new
+  </div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
