acb839a0333c4d4d6be2444d059faa5229863c4a
areum514 <21500514@handong.edu>
2020-09-21 13:58:23 +0900

    리스트 아이템 컴포넌트 공통화

--- vue-news/src/views/AskView.vue
+++ vue-news/src/views/AskView.vue
@@ -1,79 +1,18 @@
 <template>
   <div>
-   <ul class="news-list">
-      <li v-for="item in fetchedAsk" class="post" >
-        <!-- 포인트 영역 -->
-        <div class="points">
-           {{item.points}}
-        </div>
-        <!-- 기타 정보 영역 -->
-        <div>
-          <p class="news-title">
-            <router-link :to="`/item/${item.id}`">
-            {{item.title}}
-            </router-link>
-          </p>
-          <small class="link-text">
-            {{item.time_ago}} by 
-            <router-link :to="`/user/${item.user}`" class="link-text">{{item.user}}</router-link>
-          </small>
-        </div>
-      </li>
-    </ul>
+    <list-item></list-item>
   </div>
 </template>
 
 <script>
-import {mapState,mapGetters} from 'vuex';
+import ListItem from '../components/ListItem.vue'
 export default {
-  computed:{
-    //fetchedAsk를 바로 쓰고 싶다면 배열로 연결
-    ...mapGetters(['fetchedAsk',
-    ]) 
-    //#3-1 객체로 
-    /* ...mapGetters({
-      
-      fetchedAsk:'fetchedAsk',
-    }),  */
-    //#2
-    /* ...mapState({
-      ask:state=>state.ask
-    }) */
-    //#1
-    /* ask(){
-      return this.$store.state.ask;
-    } */
-  },
-  created(){
-    this.$store.dispatch('FETCH_ASK');
+  components:{
+    ListItem
   }
 }
 </script>
 
-<style scoped>
-.news-list{
-  margin: 0;
-  padding: 0;
-}
-.post{
-  list-style: none;
-  display: flex;
-  align-items: center;
-  border-bottom:1px solid #eee ;
-}
-.points{
-  width: 80px;
-  height: 60px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  color: #42b883;
+<style>
 
-}
-.news-title{
-  margin: 0;
-}
-.link-text{
-  color: #828282;
-}
 </style>
