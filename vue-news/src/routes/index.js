7a5a1d02ed7f744558fea6e7676e0b91bb23e8a6
areum514 <21500514@handong.edu>
2020-09-23 13:11:37 +0900

    hod 랑 mixin 비교

--- vue-news/src/routes/index.js
+++ vue-news/src/routes/index.js
@@ -1,5 +1,9 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import NewsView from "../views/NewsView.vue";
+import AskView from "../views/AskView.vue";
+import JobsView from "../views/JobsView.vue";
+
 import UserView from "../views/UserView.vue";
 import ItemView from "../views/ItemView.vue";
 import createListView from "../views/CreateListView.js"
@@ -17,17 +21,23 @@ export default new VueRouter({
     {
       path: "/news",
       name: "news",
-      component: createListView('NewsView'),
+      component: NewsView
+
+      // component: createListView('NewsView'),
     },
     {
       path: "/ask",
       name: "ask",
-      component: createListView('AskView'),
+      component: AskView
+
+      // component: createListView('AskView'),
     },
     {
       path: "/jobs",
       name: "jobs",
-      component: createListView('JobsView'),
+      component: JobsView
+
+      // component: createListView('JobsView'),
     },
     {
       path: "/user/:id",
